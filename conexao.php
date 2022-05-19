0b865d79e943d256acf7b1e7db394e7b5307f9d7
Rayane-Sousa <104634593+Rayane-Sousa@users.noreply.github.com>
2022-05-19 15:51:18 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php
 // Parâmetros para criar a conexão
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Site2022";
+$username = "id18586460_cadastrobd";
+$password = "dX^?~#K7hL&}[CH8";
+$dbname = "id18586460_flashservices";
 
 // Criando a conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli ($servername, $username, $password, $dbname);
 
 // Checando a conexão
 if ($conn->connect_error) {
